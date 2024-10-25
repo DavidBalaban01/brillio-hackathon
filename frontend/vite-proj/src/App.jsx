1ee5440fd7c8bd99d837edcf2b8870053aecfbaf
db_123 <david.c.balaban@gmail.com>
2024-10-25 23:03:59 +0300

    added prettier plugin

--- frontend/vite-proj/src/App.jsx
+++ frontend/vite-proj/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">Tailwind CSS is Working!</h1>
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is Working!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
